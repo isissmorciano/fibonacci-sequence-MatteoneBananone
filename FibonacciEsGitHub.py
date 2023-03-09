1b20798f8ceacfc1057cd8ed1d29b0f65ca8ba09
MatteoneBananone <123639277+MatteoneBananone@users.noreply.github.com>
2023-03-09 12:19:36 +0100

    Update FibonacciEsGitHub.py

--- FibonacciEsGitHub.py
+++ FibonacciEsGitHub.py
@@ -2,8 +2,6 @@ def fibo(num):
     if num > 1:
         return fibo(num-1) + fibo(num-2)
     return num
-
-
 num = int(input('Inserisci il numero: ' ))
 print('La tua sequenza è: ')
 for i in range(1, num+1):
